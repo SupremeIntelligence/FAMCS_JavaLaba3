1d9c9723daa2f493d4b289a153a3eb1a81c32747
Supreme Intelligence <supremeintelligence1602@mail.ru>
2024-10-05 20:42:50 +0300

    Added string input and division into tokens

--- src/main/java/org/famcs/JavaLaba3/Java_Laba3.java
+++ src/main/java/org/famcs/JavaLaba3/Java_Laba3.java
@@ -4,13 +4,25 @@
 
 package org.famcs.JavaLaba3;
 
-/**
- *
- * @author supre
- */
-public class Java_Laba3 {
+import java.util.Scanner;
 
+
+public class Java_Laba3 
+{
+    public static String strInput ()
+{   String line;
+    Scanner scan = new Scanner (System.in);
+    return scan.nextLine();
+}
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.printf("Enter the first line: \t");
+        String firstLine = strInput();
+
+        System.out.printf ("Enter the second line containing separators: \t");
+        String separatorLine = strInput();
+
+        StringEditor.Separate(firstLine, separatorLine);
+
     }
 }
