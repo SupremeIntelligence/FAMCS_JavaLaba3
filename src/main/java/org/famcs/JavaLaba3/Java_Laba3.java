2ea5e4deaef09ba70874d0891a80b1c14cde8c74
Supreme Intelligence <supremeintelligence1602@mail.ru>
2024-10-05 17:43:59 +0300

    Initial commit

---
+++ src/main/java/org/famcs/JavaLaba3/Java_Laba3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.famcs.JavaLaba3;
+
+/**
+ *
+ * @author supre
+ */
+public class Java_Laba3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
